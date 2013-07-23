c3fc00ba6b7a0dfd67f0e2adeea3fd28f6fd3f43
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2013-07-23 09:57:43 -0400

    added configurable options

--- example/example.js
+++ example/example.js
@@ -1,27 +1,39 @@
 var Progger = require('../lib/progger.js');
 
-var p1 = new Progger();
-var p2 = new Progger();
-var p3 = new Progger();
+var p1 = new Progger({
+  speed: 200,
+  token: '-',
+  color: 'red'
+});
+var p2 = new Progger({
+  speed: 100,
+  token: '*',
+  color: 'white'
+});
+var p3 = new Progger({
+  speed: 50,
+  token: '.',
+  color: 'blue'
+});
 
-console.log('start first progger');
+console.log('start first progger:');
 p1.start();
 
 setTimeout(function() {
   p1.stop();
   console.log('\n');
-  console.log('start second progger');
+  console.log('start second progger:');
   p2.start();
-}, 1500);
+}, 2000);
 
 setTimeout(function() {
   p2.stop();
   console.log('\n');
-  console.log('start third progger');
+  console.log('start third progger:');
   p3.start();
-}, 4500);
+}, 4000);
 
 setTimeout(function() {
   p3.stop();
   console.log('\n');
-}, 10500);
+}, 8000);
